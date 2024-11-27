b4e139917d41b3f3144d59df3b2259e971d3a865
Chenyu Yang <yangcyself@gmail.com>
2024-11-27 21:14:19 +0100

    refine faive dataset

--- srl_il/dataset/faive_dataset.py
+++ srl_il/dataset/faive_dataset.py
@@ -6,48 +6,88 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from glob import glob
+# from sentence_transformers import SentenceTransformer
 
 class FaiveTrajectorySequenceDataset(TrajectoryDataset):
-    def __init__(self, data_directory, onehot_goals=False):
+    def __init__(self, data_directory):
         data_directory = Path(data_directory)
-        qpos_hand = []
-        qpos_franka = []
-        oakd_front_view_images = []
-        actions_hand = []
-        actions_franka = []
-        seqlengths = []
-        for f in glob(str(data_directory / "*.h5")):
-            with h5py.File(f, "r") as h5_file:
-                actions_hand.append(torch.tensor(np.array(h5_file['actions_hand'])).float())
-                actions_franka.append(torch.tensor(np.array(h5_file['actions_franka'])).float())
-                qpos_hand.append(torch.tensor(np.array(h5_file['observations/qpos_hand'])).float())
-                qpos_franka.append(torch.tensor(np.array(h5_file['observations/qpos_franka'])).float())
-                oakd_front_view_images.append(torch.tensor(np.array(h5_file['observations/images/oakd_front_view/color'])).float().permute(0, 3, 1, 2)/255.0)
-                seq_length = actions_hand[-1].shape[0]
-                # assert seq_length == top_view_color_images.shape[0] == wrist_view_color_images.shape[0] == action.shape[0]
-                seqlengths.append(seq_length)
-        self.actions_hand = actions_hand
-        self.actions_franka = actions_franka
-        self.qpos_hand = qpos_hand
-        self.qpos_franka = qpos_franka
-        self.oakd_front_view_images = oakd_front_view_images
-        self.seqlengths = seqlengths
+        self.traj_data = None # the data in sequence
+        self.global_data = None # the information for the whole sequence
+        self.seqlengths = []
+        self._h5_files = [] 
+        # sentence_model = SentenceTransformer('all-MiniLM-L6-v2')
 
+        # Iterate over all the files to find that keys in all files
+        for f in glob(str(data_directory / "*.h5")):
+            h5_file =  h5py.File(f, "r")
+            try:
+                traj_data = {
+                    key: h5_file[key] for key in ["actions_franka", "actions_hand"] if key in h5_file
+                }
+                # encode the task description
+                global_data = {
+                    # key: sentence_model.encode([h5_file[key][()]])  for key in ["task_description"] if key in h5_file
+                }
+
+                obs_group = h5_file["observations"]
+                traj_data.update({
+                    key:  obs_group[key] for key in obs_group if key != "images"
+                })
+                for img_name, img_data in obs_group.get("images", {}).items():
+                    traj_data[f"{img_name}/color"] = img_data["color"]
+                    if "extrinsics" in img_data:
+                        global_data[f"{img_name}/extrinsics"] = img_data["extrinsics"]
+                    if "intrinsics" in img_data:
+                        global_data[f"{img_name}/intrinsics"] = img_data["intrinsics"]
+                    if "projection" in img_data:
+                        global_data[f"{img_name}/projection"] = img_data["projection"]
+                self._h5_files.append(h5_file)
+            except Exception as e:
+                print(f"Warning: skipping {f}")
+                print(e)
+                continue
+            
+            if self.traj_data is None:
+                self.traj_data = {key: [data] for key, data in traj_data.items()}
+                self.global_data = {key: [data] for key, data in global_data.items()}
+
+            else: # find the intersection of keys
+                for key in list(self.traj_data.keys()):
+                    if key in traj_data:
+                        self.traj_data[key].append(traj_data[key])
+                    else:
+                        self.traj_data.pop(key)
+                        print(f"Warning: key {key} not found in {f}")
+                for key in list(self.global_data.keys()):
+                    if key in global_data:
+                        self.global_data[key].append(global_data[key])
+                    else:
+                        self.global_data.pop(key)
+                        print(f"Warning: key {key} not found in {f}")
+            self.seqlengths.append(len(list(traj_data.values())[0]))
+        
     def get_seq_length(self, idx):
         return int(self.seqlengths[idx])
 
     def __getitem__(self, idx):
-        return ({"actions_hand":self.actions_hand[idx],
-                 "actions_franka":self.actions_franka[idx],
-                 "qpos_hand":self.qpos_hand[idx],
-                 "qpos_franka":self.qpos_franka[idx],
-                 "oakd_front_view_images":self.oakd_front_view_images[idx]}, 
-                None)
+        return {k:v[idx] for k, v in self.traj_data.items()}, {k:v[idx] for k, v in self.global_data.items()}
 
     def __len__(self):
-        return len(self.actions_franka)
+        return len(self.seqlengths)
+
+    def __del__(self):
+        try:
+            for h5_file in self._h5_files:
+                h5_file.close()
+        except:
+            pass # it's okay
 
     def load(self, data, key, is_global=False):
+        if key == "task_description":
+            return data
+        data = torch.tensor(np.array(data)).float()
+        if "color" in key:
+            data = data.permute(0, 3, 1, 2) / 256.0
         return data
 
 
